16df20a6d537877d0a8392411e70bbff3d26ded5
Stephen <venter76@gmail.com>
2023-09-19 14:36:57 +0200

    2

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = 'static-cache-v3';
 const STATIC_ASSETS = [
+    'placeholder.ejs',
     '/iconLarge_1.png',
     '/iconLarge_2.png',
     '/iconLarge_3.png',
@@ -32,21 +33,38 @@ self.addEventListener('fetch', (event) => {
 
     if (dynamicPaths.some(path => event.request.url.includes(path))) {
         // Use Network Only strategy for dynamic content
-        event.respondWith(fetch(event.request));
-    } else {
+    //     event.respondWith(fetch(event.request));
+    // } else {
         // Use Cache First strategy for static assets
         event.respondWith(
-            caches.match(event.request).then((response) => {
-                return response || fetch(event.request).then((fetchResponse) => {
-                    return caches.open(CACHE_NAME).then((cache) => {
-                        cache.put(event.request, fetchResponse.clone());
-                        return fetchResponse;
-                    });
-                });
-            })
-        );
-    }
-});
+
+            fetch(event.request).catch(() => {
+                // Return placeholder.html if the app is offline and the request is for the homepage
+                if (event.request.url.includes('/')) {
+                    return caches.match('/placeholder.ejs');
+                }
+
+            // caches.match(event.request).then((response) => {
+            //     return response || fetch(event.request).then((fetchResponse) => {
+            //         return caches.open(CACHE_NAME).then((cache) => {
+            //             cache.put(event.request, fetchResponse.clone());
+            //             return fetchResponse;
+                    })
+                );
+            } else {
+                // Use Cache First strategy for static assets
+                event.respondWith(
+                    caches.match(event.request).then((response) => {
+                        return response || fetch(event.request).then((fetchResponse) => {
+                            return caches.open(CACHE_NAME).then((cache) => {
+                                cache.put(event.request, fetchResponse.clone());
+                                return fetchResponse;
+                            });
+                        });
+                    })
+                );
+            }
+        });
     
     
     
